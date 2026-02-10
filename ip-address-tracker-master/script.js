5544771cd625252efb162d4a33095926cc80b929
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-02-09 23:25:19 -0500

    removed zoom controls from map

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -5,7 +5,7 @@ function test() {
 
 test();
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map', {zoomControl: false}).setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
